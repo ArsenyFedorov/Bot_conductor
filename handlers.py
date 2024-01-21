e33b36b097c015b1f7ae51cde35e08b52e66a769
Это_gambit <ars071026@gmail.com>
2024-01-21 21:58:40 +0500

    Добавил картонок к сообщениям(теперь всё как то живенько даже )

--- handlers.py
+++ handlers.py
@@ -1,27 +1,48 @@
 from aiogram import Router, F, Bot
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, InputFile
 from aiogram.filters import Command
 from keyboard import *
 from text import *
+import emoji
 
 handlers_router = Router()
 
 
 @handlers_router.message(Command("start"))
-async def com_start(message: Message):
-    await message.answer(text=greeting, reply_markup=kb_start())
+async def com_start(message: Message, bot: Bot):
+    await bot.send_photo(chat_id=message.from_user.id,
+                         photo="https://i.pinimg.com/550x/5e/e8/39/5ee839d9f00ad2d81ece2f1d5c30ae5e.jpg",
+                         caption=greeting, reply_markup=kb_start())
 
 
 @handlers_router.callback_query(F.data == "signUp")
-async def com_start(callback: CallbackQuery, bot: Bot):
-    await bot.send_message(callback.from_user.id, text=sign_up_text, reply_markup=kb_sign_up_day())
+async def sign_up(callback: CallbackQuery, bot: Bot):
+    await bot.send_photo(callback.from_user.id,
+                         photo="https://imageru.ru/uploads/posts/2016-05/1464711563_imageru.ru"
+                               "_business-men-psd.jpg",
+                         caption=sign_up_text, reply_markup=kb_sign_up_day())
 
 
 @handlers_router.callback_query(F.data == "schedule")
-async def com_start(callback: CallbackQuery, bot: Bot):
-    await bot.send_message(callback.from_user.id, text=schedule_text, reply_markup=kb_schedule())
+async def schedule(callback: CallbackQuery, bot: Bot):
+    await bot.send_photo(callback.from_user.id, photo="https://kpfu.ru/portal/docs/F1590481391/1422605698.jpg",
+                         caption=schedule_text, reply_markup=kb_schedule())
+
+
+@handlers_router.callback_query(F.data == "setting")
+async def setting(callback: CallbackQuery, bot: Bot):
+    await bot.send_photo(callback.from_user.id,
+                         photo="https://img.freepik.com/premium-vector/ringing-alarm-clock-with"
+                               "-two-bells-illustration-cartoon-isolated-dinamic-image-green-and"
+                               "-yellow-color-morning-alarm-clock_124848-425.jpg",
+                         caption=setting_text, reply_markup=kb_setting())
 
 
 @handlers_router.callback_query(F.data == "day")
 async def day_class(callback: CallbackQuery, bot: Bot):
     await bot.send_message(callback.from_user.id, text="ARBUZIKI")
+
+
+@handlers_router.callback_query(F.data == "oclock")
+async def oclock(callback: CallbackQuery, bot: Bot):
+    await bot.send_message(callback.from_user.id, text="BANANCHIKI")
--- keyboard.py
+++ keyboard.py
@@ -6,6 +6,7 @@ def kb_start():
     keyboard = InlineKeyboardBuilder()
     keyboard.button(text=sign_up, callback_data="signUp")
     keyboard.button(text=schedule, callback_data="schedule")
+    keyboard.button(text=setting, callback_data="setting")
     keyboard.adjust(1)
     return keyboard.as_markup()
 
@@ -19,10 +20,21 @@ def kb_sign_up_day():
     keyboard.button(text=friday, callback_data="day")
     keyboard.button(text=saturday, callback_data="day")
     keyboard.button(text=sunday, callback_data="day")
+    keyboard.adjust(2, 2, 2, 1)
     return keyboard.as_markup()
 
 
 def kb_schedule():
     keyboard = InlineKeyboardBuilder()
     keyboard.button(text=sign_up, callback_data="signUp")
+    return keyboard.as_markup()
+
+
+def kb_setting():
+    keyboard = InlineKeyboardBuilder()
+    keyboard.button(text=morning, callback_data="oclock")
+    keyboard.button(text=dinner, callback_data="oclock")
+    keyboard.button(text=day, callback_data="oclock")
+    keyboard.button(text=evening, callback_data="oclock")
+    keyboard.adjust(2, 2)
     return keyboard.as_markup()
